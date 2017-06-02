61ca7ba5a1a5a012ec6670ad15994880c3a72107
Rena Rallis <renarallis@msn.com>
2017-06-02 09:57:12 -0700

    add document ready function to include variables

---
+++ Desktop/track-suggester/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+$("#form#survey").submit(function(event) {
+    event.preventionDefault();
+    var experience = $("select#experience").val();
+    var learning = $("select#learning").val();
+    var preference = $("select#preference").val();
+    var environment = $("select#environment").val();
+    var web-apps = $("select#web-apps").val();
+  }
+})
