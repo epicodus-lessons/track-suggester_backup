ac22120e2e454a17ae6d5d9863753ef1416c933e
Rena Rallis <renarallis@msn.com>
2017-06-02 15:32:11 -0700

    modify css styling

--- Desktop/track-suggester/js/scripts.js
+++ Desktop/track-suggester/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-$("#form#survey").submit(function(event) {
+  $("#form#survey").submit(function(event) {
     event.preventionDefault();
     var experience = $("select#experience").val();
     var learning = $("select#learning").val();
@@ -7,15 +7,19 @@ $("#form#survey").submit(function(event) {
     var environment = $("select#environment").val();
     var web-apps = $("select#web-apps").val();
 
-    if (preference === "Back-End" && learning === "yes");
-      alert("Check out PHP!!"); }
-      $("PHP").show();
+    if (preference === "Back-End" && learning === "yes") {
+        $("#PHP").show();
+        alert("Check out PHP!!");
+      }
+
     } else if (environment === "Yes" && experience === novice) {
+      $("#C-sharp").show();
       alert("Give C# a try.");
-      $("C-sharp").show();
+    }
+
     } else if (web-apps === "Yes" && experience = none) {
-      alert("You will like Ruby!"); }
-      $("Ruby").show();
+      $("#Ruby").show();
+      alert("You will like Ruby!");
     }
   });
 });
