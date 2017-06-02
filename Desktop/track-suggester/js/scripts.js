dc42c280bf804c2b351592e12f67e6bdee2e2610
Rena Rallis <renarallis@msn.com>
2017-06-02 16:38:32 -0700

    add bug report to project directory to determine how directory path changed

--- Desktop/track-suggester/js/scripts.js
+++ Desktop/track-suggester/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#form#survey").submit(function(event) {
+  $("#form#survey").submit(function() {
     event.preventionDefault();
     var experience = $("select#experience").val();
     var learning = $("select#learning").val();
@@ -8,18 +8,15 @@ $(document).ready(function() {
     var web-apps = $("select#web-apps").val();
 
     if (preference === "Back-End" && learning === "yes") {
-        $("#PHP").show();
-        alert("Check out PHP!!");
-      }
+      $("#php").show();
+    }
 
     } else if (environment === "Yes" && experience === novice) {
-      $("#C-sharp").show();
-      alert("Give C# a try.");
+      $("#c-sharp").show();
     }
 
     } else if (web-apps === "Yes" && experience = none) {
-      $("#Ruby").show();
-      alert("You will like Ruby!");
+      $("#ruby").show();
     }
   });
 });
